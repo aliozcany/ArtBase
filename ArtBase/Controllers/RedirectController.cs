192a33a9eeb5925315346ec7ca7e7a354fa79849
ALİ ÖZCAN YILMAZ <aliozcany6103@gmail.com>
2025-01-04 12:05:49 +0300

    Book.api's RedirectController added

---
+++ ArtBase/Controllers/RedirectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtBase.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RedirectController : ControllerBase
+    {
+        [HttpGet("to-vue")]
+        public IActionResult RedirectToVue()
+        {
+            return Redirect("http://localhost:5000"); // Vue.js uygulamanın çalıştığı URL
+        }
+    }
+}
