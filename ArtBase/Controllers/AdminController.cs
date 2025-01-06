07730584ef88e17bbaed652c3d4fbac6f04c4366
ALİ ÖZCAN YILMAZ <aliozcany6103@gmail.com>
2025-01-06 22:58:55 +0300

    kullanıcı silme özelliği getirildi ve cascading ayarlandı

---
+++ ArtBase/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using ArtBase.Models;
+using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtBase.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager; // Kullanıcı kimliği yönetimi
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> ShowAllUsers()
+        {
+            var userid = _userManager.GetUserId;
+            ViewBag.AdminID = userid;
+            var users = _userManager.Users.ToList();
+            
+            return View(users);
+        }
+
+        // Kullanıcıyı silmek için bir aksiyon
+        [HttpPost]
+        public async Task<IActionResult> Delete(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound("Kullanıcı bulunamadı.");
+            }
+            //kullanıcıyı sil
+            var result = await _userManager.DeleteAsync(user);
+            if (!result.Succeeded)
+            {
+                return BadRequest("Kullanıcı silinemedi.");
+            }
+
+            return RedirectToAction("ShowAllUsers");
+        }
+    }
+}
